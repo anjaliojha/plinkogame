7d37b3d2dac4cd851100323636d8490f7e498015
Prachi106-star <75433997+Prachi106-star@users.noreply.github.com>
2021-02-24 19:43:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+ 
+var engine , world ;
+var ground ;
+function preload(){
+  
+}
+
+
+function setup() {
+ var canvas =  createCanvas(480,800);
+ // createSprite(400, 200, 50, 50);
+
+
+ var engine = Engine.create();
+ var world = engine.world;
+  
+ ground = new Ground(100,10,100,10);
+ //divider1 = new Division(200,200,300,10)
+ //divider2 = new Division(400,200,300,10)
+ //divider3 = new Division(100,200,300,10)
+}
+
+function draw() {
+  Engine.uptade(engine);
+  background(0,0,0);  
+  drawSprites();
+
+  ground.display();
+  /*divider1.display();
+  divider2.display();
+  divider3.display();*/
+}
